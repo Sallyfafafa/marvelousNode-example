92f2dfca237310c3a7aba1dbdfae109fdbb78aa6
Sally <sally@SallydeMacBook-Pro.local>
2020-03-04 13:37:10 +0800

    websocket

--- websocket/server.js
+++ websocket/server.js
@@ -1,8 +1,26 @@
 const express  = require("express");
-const wsio  = require("websocket.io");
+const ws  = require("ws");
 
 var app = express();
-var ws = wsio.attach(app);
+app.use(express.static('src'));
+var wss = new ws.Server({port: 8080});
 
-wx.on(connection, )
+wss.on('connection', function connection(ws){
+    console.log('server: receive connection.');
+    
+    ws.on('message', function incoming(message) {
+        console.log('server: received: %s', message);
+        ws.send('word message');
+    });
 
+    ws.on('close', function(){
+        console.log('server: closed');
+    })
+    ws.send('word');
+})
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/src/index.html');
+});
+  
+app.listen(3000);
