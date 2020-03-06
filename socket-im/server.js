019ce045609edaf38cef6a67cbae68c7bbc578b4
Sally <sally@SallydeMacBook-Pro.local>
2020-03-06 11:26:20 +0800

    聊天

--- socket-im/server.js
+++ socket-im/server.js
@@ -9,19 +9,21 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     console.log('get a connection');
     io.emit('hasConnect', '有人连接进来了');
-    socket.on('disconnect', function(){ // 关闭连接触发
-        io.emit('broadcast', '有人关闭连接');
-    });
-    socket.on('chat message', function(msg){ // 收到客户端的消息
-        console.log('get massage:', msg);
-    });
-    socket.on('sendMsg', function(msg, id){
-        console.log(msg, id);
-        io.emit('receiveMsg', msg, id);
-    })
+
     socket.on('chat another', function(msg){ // 收到客户端的消息
-        io.emit('chat another', 'this is a message to everybody'); // 在发给客户端
+        io.emit('broadcast', msg);
     });
+
+    // socket.on('disconnect', function(){ // 关闭连接触发
+    //     io.emit('broadcast', '有人关闭连接');
+    // });
+    // socket.on('chat message', function(msg){ // 收到客户端的消息
+    //     console.log('get massage:', msg);
+    // });
+    // socket.on('sendMsg', function(msg, id){
+    //     console.log(msg, id);
+    //     io.emit('receiveMsg', {msg, id});
+    // })
 })
 
 http.listen(3000, function(){ // 监听端口
